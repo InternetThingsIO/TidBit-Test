e3e8e63e2f186e6ada923400ce440212fd68a8d5
Jason <jason@internetthings.io>
2015-03-26 00:20:08 -0400

    First working copy of the Tidbit test program.  Currently the program can only create and cancel notifications.  Version 0.1

--- app/src/main/java/io/internetthings/tidbittest/MainActivity.java
+++ app/src/main/java/io/internetthings/tidbittest/MainActivity.java
@@ -10,18 +10,30 @@ package io.internetthings.tidbittest;
  This program will create and cancel notifications.
 
  */
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    private NotificationManager nManager = null;
+    private int nId = 0;
+    private String sTag = "io.internetthings.tidbittest";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        nManager = (NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
     }
 
 
@@ -45,5 +57,32 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    //Function that performs an action when a button is pressed
+    public void buttonClicked(View v){
+        //Action performed when Create Notification button is pressed
+        if(v.getId() == R.id.btnCreate){
+            nId = (int)System.currentTimeMillis();
+            NotificationCompat.Builder nCompat = new NotificationCompat.Builder(this);
+            nCompat.setContentTitle("Notification Created");
+            nCompat.setContentText("Test Notification Id: " + nId);
+            nCompat.setSmallIcon(R.mipmap.ic_launcher);
+            nManager.notify(sTag, nId, nCompat.build());
+            toastMSG("Notification Created");
+        }
+        //Action performed when Clear All Notification button is pressed
+        else if(v.getId() == R.id.btnClearAll){
+           nManager.cancelAll();
+           toastMSG("Notifications Cancelled");
+        }
+    }
+    //Creates toast messages
+    public void toastMSG(CharSequence text){
+        Context context = getApplicationContext();
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
     }
 }
